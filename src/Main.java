c7e9fc16bdbd0c9f0cf7460e328d797e5f2a1b9a
dimsuwr <soup_95@mail.ru>
2020-07-02 14:26:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+
+    static Object block = new Object();
+    static volatile int currentNum = 1;
+    static final  int num = 5;
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            try{
+                for (int i = 0; i < num ; i++) {
+                    synchronized (block){
+                        while (currentNum != 1){
+                            block.wait();
+                        }
+                        System.out.println("А ");
+                        currentNum = 2;
+                        block.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+
+
+        new Thread(()->{
+            try{
+                for (int i = 0; i < num ; i++) {
+                    synchronized (block){
+                        while (currentNum != 2){
+                            block.wait();
+                        }
+                        System.out.println("B ");
+                        currentNum = 3;
+                        block.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+
+
+
+
+
+        new Thread(()->{
+            try{
+                for (int i = 0; i < num ; i++) {
+                    synchronized (block){
+                        while (currentNum != 3){
+                            block.wait();
+                        }
+                        System.out.println("C ");
+                        currentNum = 1;
+                        block.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
